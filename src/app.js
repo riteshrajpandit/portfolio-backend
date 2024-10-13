2efbbbc8c8e518e9ca0c1453845be0056fb9d246
Ritesh Raj Pandit <41156634+riteshrajpandit@users.noreply.github.com>
2024-10-13 10:09:55 +0545

    updated to mongo from postgresql

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+
+const connectDB = require('./config/database');
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -12,9 +14,11 @@ const socialRoutes = require('./routes/socialRoutes');
 
 const app = express();
 
+connectDB();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/blog', blogRoutes);
--- src/config/database.js
+++ src/config/database.js
@@ -1,51 +1,12 @@
-const { Sequelize } = require('sequelize');
-const bcrypt = require('bcryptjs');
-require('dotenv').config();
+const mongoose = require("mongoose");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-});
-
-// Import models
-const User = require('../models/User')(sequelize);
-const Blog = require('../models/Blog')(sequelize);
-const Project = require('../models/Project')(sequelize);
-const Message = require('../models/Message')(sequelize);
-const Social = require('../models/Social')(sequelize);
-
-// Initialize models
-const models = {
-  User,
-  Blog,
-  Project,
-  Message,
-  Social
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database connected");
+  } catch (error) {
+    console.log(`Mongodb error ${error}`);
+  }
 };
 
-async function initializeDatabase() {
-  try {
-    // Connect to the database
-    await sequelize.authenticate();
-    console.log('Connected to the database.');
-
-    // Create tables
-    await sequelize.sync({ force: true }); // Be careful with {force: true} in production!
-    console.log('Database synchronized.');
-
-    // Create admin user
-    const adminPassword = process.env.ADMIN_PASSWORD || 'admin123';
-    const hashedPassword = await bcrypt.hash(adminPassword, 10);
-    await User.create({
-      username: 'admin',
-      password: hashedPassword,
-      isAdmin: true
-    });
-    console.log('Admin user created.');
-
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
-
-module.exports = { sequelize, initializeDatabase, models };
+module.exports = connectDB;
